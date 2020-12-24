92c41186d8f7982904485582cdc61d7b53c689cc
Laurent Montigny <lmontigny@users.noreply.github.com>
2020-12-24 17:08:33 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -4,4 +4,9 @@ conda create -n rlib
 conda activate rlib
 
 pip install 'ray[rllib]'
+
+# Proximal Policy Optimization (PPO)
 rllib train --run=PPO --env=CartPole-v0 
+
+# Deep Q Networks (DQN
+rllib train --run DQN --env CartPole-v0 --checkpoint-freq 10
